72123cd884ec176607db878c62d2016a0c6e87ae
bsolis6878 <bsolis6878@gmail.com>
2022-10-19 20:03:43 -0500

    added basic routes to display pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,33 @@
 const router = require('express').Router();
 
+// render homepage
 router.get('/', (req, res) => {
     res.render('homepage');
-})
+});
+
+// renders signup page
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+// renders login page
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// renders employee page
+router.get('/employee', (req, res) => {
+    res.render('employee');
+});
+
+// renders entrepreneur page
+router.get('/entrepreneur', (req, res) => {
+    res.render('entrepreneur');
+});
+
+// renders team lead page
+router.get('/team-lead', (res, res) => {
+    res.render('team-lead')
+});
 
 module.exports = router;
