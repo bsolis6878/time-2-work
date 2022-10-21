afeab28e813e2d9e3f7da30b2f9b8cdfeea5405f
bsolis6878 <bsolis6878@gmail.com>
2022-10-21 12:20:04 -0500

    added session cookie to server

--- server.js
+++ server.js
@@ -7,20 +7,37 @@ const helpers = require('./utils/helpers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// enables handlebars
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// turn on routes
+// enable session cookies
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+// activates routes
 app.use(routes);
 
-// turn on connection to db and server
+// activates connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
