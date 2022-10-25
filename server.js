e0cf25c1ebe7d206c56ec69d63f49299a90ce289
brenbrenda55 <108436173+brenbrenda55@users.noreply.github.com>
2022-10-24 21:03:21 -0700

    nar bar 2 R

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.use(session(sess));
 app.use(routes);
 
 // activates connection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
