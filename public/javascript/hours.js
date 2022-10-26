fa6fea8eeac4b340e515f5294c5d3da99108af3d
brenbrenda55 <108436173+brenbrenda55@users.noreply.github.com>
2022-10-25 22:58:48 -0700

    updte

--- public/javascript/hours.js
+++ public/javascript/hours.js
@@ -1,20 +1,18 @@
-async function editUserHandler(event) {
+async function hoursHandler(event) {
   event.preventDefault();
 
-  const company_id = document.querySelector('input[name="username-edit"]').value.trim();
-  const employee_id = document.querySelector('input[name="email-edit"]').value.trim();
-  const job_id = document.querySelector('input[name="password-edit"]').value.trim();
-  const hours_worked = document.querySelector('input[name="data-id"]').value.trim();
-  const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
-  ];
-  const response = await fetch(`/api/timglog/`, {
+  const company_id = document.querySelector('#company_id').value.trim();
+  const employee_id = document.querySelector('#employee_id').value.trim();
+  const job_id = document.querySelector('#job-id').value.trim();
+  const minutes_worked = document.querySelector('#minutes-worked').value.trim();
+ 
+  const response = await fetch(`/api/timelog`, {
     method: 'POST',
     body: JSON.stringify({
     company_id,
     employee_id,
     job_id,
-    hours_worked
+    minutes_worked
     }),
     headers: {
       'Content-Type': 'application/json'
@@ -28,4 +26,4 @@ async function editUserHandler(event) {
   }
 }
 
-document.querySelector('.edit-form').addEventListener('submit', editUserHandler);
+document.querySelector('.jobs-form').addEventListener('submit', hoursHandler);
